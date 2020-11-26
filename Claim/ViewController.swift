931d1d1b40037e2e84581104f9d5e3a09c2d57df
Hung Cun <hungcun1996@csu.fullerton.edu>
2020-11-25 21:01:09 -0800

    Initial Commit

---
+++ Claim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Claim
+//
+//  Created by Hung Cun on 11/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
